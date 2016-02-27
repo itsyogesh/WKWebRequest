3bc16fc9b5ff24ba38ef361af1674efaf6b382b8
Yogesh Kumar <yogeshkumar180592@gmail.com>
2016-02-27 23:16:53 +0530

    Added the wkwebview screen, including the bounds

--- WebRequest/ViewController.swift
+++ WebRequest/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        container.addSubview(webView)
+        
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        let urlStr = "https://developer.apple.com/swift/blog"
+        let url = NSURL(string: urlStr)!
+        
+        let request = NSURLRequest(URL: url)
+        
+        webView.loadRequest(request)
+    }
+    
+    @IBAction func loadSwift(sender: AnyObject){
+        
+    }
+    
+    @IBAction func loadGossip(sender: AnyObject){
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
